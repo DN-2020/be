dea3a3fdcfa5401801cd94d0c47af77c71ad68c8
chan <lsc4237@gmail.com>
2020-10-05 22:59:01 +0900

    [update] review to 0.1.0

---
+++ src/main/java/com/db2020/pj/service/ReviewService.java
@@ -0,0 +1,12 @@
+package com.db2020.pj.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface ReviewService {
+    public void insertReview(HashMap<String, Object> map);
+    public List<HashMap<String, Object>> selectListReview(HashMap<String, Object> map );
+    public List<HashMap<String, Object>> selectUserReview(HashMap<String, Object> map);
+    public HashMap<String, Object> selectDetailReview(HashMap<String, Object> map);
+    public void updateReview(HashMap<String,Object> map);
+}
