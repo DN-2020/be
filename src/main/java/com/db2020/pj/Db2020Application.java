bbbbcb559b9f96e8dac80b5a4cfa6ec6ec322ce3
wishoon <heonga2@gmail.com>
2020-10-09 15:59:32 +0900

    [Update] .gitignore 추가

--- src/main/java/com/db2020/pj/Db2020Application.java
+++ src/main/java/com/db2020/pj/Db2020Application.java
@@ -3,10 +3,12 @@ package com.db2020.pj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@PropertySource("classpath:application-production.properties")
 public class Db2020Application {
 
 	public static void main(String[] args) {
