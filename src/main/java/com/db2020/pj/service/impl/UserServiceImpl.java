bcd824bb1e955c9cde8db4be3e121f50e1645120
wishoon <heonga2@gmail.com>
2020-11-03 14:20:06 +0900

    [Fix] 오류 수정

--- src/main/java/com/db2020/pj/service/impl/UserServiceImpl.java
+++ src/main/java/com/db2020/pj/service/impl/UserServiceImpl.java
@@ -14,42 +14,41 @@ import com.db2020.pj.service.UserService;
 @Service
 public class UserServiceImpl implements UserService {
 
-	@Autowired
-	UserRepository userRepository;
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-	
-	@Override
-	public Customer userInfo(String email) {
-	
-		Customer user = userRepository.findUserInfo(email);
-		
-		return user;
-	}
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-	@Override
-	public void userInfo(Customer user) {
+    @Override
+    public Customer userInfo(String email) {
 
-		userRepository.reviseUserInfo(Customer.builder()
-				.customer_seq(user.getCustomer_seq())
-				.customer_email(user.getCustomer_email())
-				.customer_pw(passwordEncoder.encode(user.getCustomer_pw()))
-				.customer_nm(user.getCustomer_nm())
-				.customer_tel(user.getCustomer_tel())
-				.customer_post(user.getCustomer_post())
-				.customer_address(user.getCustomer_address())
-				.customer_detail_address(user.getCustomer_detail_address()).build());
-		
-	}
+        Customer user = userRepository.findUserInfo(email);
 
-	@Override
-	public void removeUser(String customer_email) {
-		
-		userRepository.removeUser(customer_email);
-	}
+        return user;
+    }
 
-	@Override
-	public int selectUserSeq(String email) {
-		return userRepository.customerSeq(email);
-	}
+    @Override
+    public void userInfo(Customer user) {
+
+        userRepository.reviseUserInfo(Customer.builder()
+//                .customer_seq(user.getCustomer_seq())
+                .customer_email(user.getCustomer_email())
+                .customer_pw(passwordEncoder.encode(user.getCustomer_pw()))
+                .customer_nm(user.getCustomer_nm())
+                .customer_tel(user.getCustomer_tel())
+                .customer_post(user.getCustomer_post())
+                .customer_address(user.getCustomer_address())
+                .customer_detail_address(user.getCustomer_detail_address()).build());
+    }
+
+    @Override
+    public void removeUser(String customer_email) {
+
+        userRepository.removeUser(customer_email);
+    }
+
+    @Override
+    public int selectUserSeq(String email) {
+        return userRepository.customerSeq(email);
+    }
 }
