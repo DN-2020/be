0690a8a86af46e9addc1092324ff745d03af6ac3
wishoon <heonga2@gmail.com>
2020-11-12 00:36:13 +0900

    [Log] 로그 추가

--- src/main/java/com/db2020/pj/config/cookie/CookieUtil.java
+++ src/main/java/com/db2020/pj/config/cookie/CookieUtil.java
@@ -19,6 +19,11 @@ public class CookieUtil {
 	}
 	
 	public Cookie getCookie(HttpServletRequest req, String cookieName){
+		System.out.println(req.getProtocol());
+		System.out.println(req.getRequestURL());
+		System.out.println(req.getHeader("Set-Cookie"));
+		System.out.println(req.getHeader("accessToken"));
+		System.out.println(req.getHeaderNames());
 		System.out.println("전송 결과: " + req.toString());
 		System.out.println("쿠키 이름: " + cookieName);
         final Cookie[] cookies = req.getCookies();
