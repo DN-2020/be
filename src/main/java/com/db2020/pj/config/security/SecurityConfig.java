219dadc6df92b9ebcff2b019617d74ed3bc579c0
wishoon <heonga2@gmail.com>
2020-11-13 22:11:47 +0900

    [Update] 오타 수정

--- src/main/java/com/db2020/pj/config/security/SecurityConfig.java
+++ src/main/java/com/db2020/pj/config/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 	       		  .authorizeRequests()
 //				  .antMatchers("/**").permitAll()
-	       		  .antMatchers(HttpMethod.OPTIONS, "/api/v1/**").permitAll() 
+	       		  .antMatchers(HttpMethod.OPTIONS, "/v1/**").permitAll() 
 	       		  .antMatchers("/**/admin/**").hasRole("ADMIN")
 	       		  .antMatchers("/**/company/**").hasAnyRole("ADMIN", "EMP")
 	       		  .antMatchers("/exception/**").permitAll()
