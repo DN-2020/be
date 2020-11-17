ced19e9555fca963051fcef5dfd89ce85be0cb9c
chan <lsc4237@gmail.com>
2020-11-17 18:46:05 +0900

    [Fix] image file명 변경

--- src/main/java/com/db2020/pj/service/S3Service.java
+++ src/main/java/com/db2020/pj/service/S3Service.java
@@ -47,13 +47,10 @@ public class S3Service {
     public String upload(MultipartFile file, String name) throws IOException {
         String fileName = name + file.getOriginalFilename();
 
-        System.out.println("왜앙대");
-
         s3Client.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), null)
                 .withCannedAcl(CannedAccessControlList.PublicRead));
 
-        System.out.println("진짜왜앙ㅇ대");
-        return s3Client.getUrl(bucket, fileName).toString();
+        return fileName;
     }
 
     public String upload(MultipartFile file) throws IOException {
