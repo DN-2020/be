7fd2ae65028108572f711fedf43912e01f772db0
wishoon <heonga2@gmail.com>
2020-11-18 03:13:57 +0900

    [Update] API ver2.0

--- src/main/java/com/db2020/pj/repository/GoodsRepository.java
+++ src/main/java/com/db2020/pj/repository/GoodsRepository.java
@@ -10,29 +10,29 @@ import com.db2020.pj.entity.GoodsDetail;
 
 public interface GoodsRepository {
 	
-	public void register(Map<String, String> goods);
+//	public void register(Map<String, String> goods);
 	
-	public void detail_register(Map<String, String> detail_goods);
+	public void detail_register(GoodsDetail goodsDetail);
 	
-	public Goods selectOne(int goods_seq);
+//	public Goods selectOne(int goods_seq);
 	
 	public GoodsDetail selectOne(HashMap<String, Object> query);
 	
 //	public Map<String, Object> reserve_date(Map<String, Object> reserve_date);
+//
+	public List<GoodsDetail> selectList(Map<String, Object> parameter);
 	
-	public List<Goods> selectList(Map<String, Object> parameter);
-	
-	public List<Goods> selectAdminList(Map<String, Object> parameter);
+	public List<GoodsDetail> selectAdminList(Map<String, Object> parameter);
 	
 	public List<GoodsDetail> selectDetailList(int goods_seq);
 	
 	public void goodsIsView(Map<String, Object> parameter);
 	
-	public void goodsDetailIsView(Map<String, Object> parameter, String goods_detail_view_yn);
+//	public void goodsDetailIsView(Map<String, Object> parameter, String goods_detail_view_yn);
 	
 	public void update(Map<String, Object> goods);
 	
-	public void detail_update(Map<String, Object> goods);
+//	public void detail_update(Map<String, Object> goods);
 	
 	public void delete(String goods_seq);
 	
