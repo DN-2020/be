22d0ec0afd48b4e63c15e1f0825fd15b0048ab8f
wishoon <heonga2@gmail.com>
2020-11-18 21:46:30 +0900

    [Fix] goodsType 추가

---
+++ src/main/java/com/db2020/pj/entity/GoodsType.java
@@ -0,0 +1,15 @@
+package com.db2020.pj.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class GoodsType {
+
+    int type_seq;
+    String type_nm;
+
+}
